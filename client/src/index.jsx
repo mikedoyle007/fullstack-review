471e1572c52ad20f9374ff6dd6789f75dac8a3c0
Mike Doyle <mikedoyle007@gmail.com>
2017-07-28 15:09:11 -0700

    Successfully send post req to server

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import Search from './components/Search.jsx';
 import RepoList from './components/RepoList.jsx';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,12 +11,32 @@ class App extends React.Component {
     this.state = { 
       repos: []
     }
-
+    this.search = this.search.bind(this);
   }
 
   search (term) {
     console.log(`${term} was searched`);
-    // TODO
+
+    // POST 'term' to server
+    axios.post('http://127.0.0.1:1128/repos/import', {
+      username: term
+    })
+    .then((response) => {
+      console.log(`${term} has been accepted`);
+      console.log('response: ', response);
+    })
+    .catch((err) => {
+      console.log(`something went wrong in the POST request of ${term}`);
+      console.log('error:', err);
+    });
+
+    // axios.get(`https://api.github.com/users/${term}`)
+    //   .then((response) => {
+    //     console.log('response: ', response);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
   }
 
   render () {
--- server/index.js
+++ server/index.js
@@ -1,16 +1,30 @@
 var express = require('express');
+let bodyParser = require('body-parser');
 
 var app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.post('/repos/import', function (req, res) {
   // TODO
+  console.log('post request received by server');
+  console.log('request is:', req.body);
 });
+
+// app.post('/', (req, res) => {
+//   console.log('request is: ', req);
+//   res.send('success');
+// });
+
 
 app.get('/repos', function (req, res) {
   // TODO
 });
+
+
 
 var port = 1128;
 
