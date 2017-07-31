9eef02739de558539bcb18e2c2fbd8b6a6b9e212
Mike Doyle <mikedoyle007@gmail.com>
2017-07-30 19:38:44 -0700

    Display repo name, author, and description

--- client/src/components/Repo.jsx
+++ client/src/components/Repo.jsx
@@ -1,32 +1,17 @@
 import React from 'react';
 
 const Repo = (props) => {
+
   return (
     <div>
       <br/>
       <div>
-        repo: {props.repos.name}
+        <p>repo: <a href={props.repos.url}>{props.repos.name}</a></p>
+        <p> by: {props.repos.username}</p>
+        <p>description: {props.repos.description}</p>
       </div>
     </div>
   );
 }
 
 export default Repo;
-
-/*
-  <br/>
-  <p>repo: <a href={props.repos.url}>{props.repos.name}</a></p>
-  <p>by: {props.repos.owner.login}</p>
-  <p>{props.repos.description}</p>        
-
-  this works: 
-
-  return (
-    <div>
-      <br/>
-      <li>
-        repo: {props.repos.name}
-      </li>
-    </div>
-  );
-*/
